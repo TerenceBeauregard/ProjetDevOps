b9d8639b1b970c52f098684ec53ec49ef2d8010c
TerenceBeauregard <beauregardterence@gmail.com>
2026-06-14 11:54:14 +0200

    fix compilation error

---
+++ src/main/java/ytg/projetjavaytg/dto/ApprentiFormDTO.java
@@ -0,0 +1,22 @@
+package ytg.projetjavaytg.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApprentiFormDTO {
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String programme;
+    private String anneeAcademique;
+    private String majeure;
+    private String niveau;
+    private String missionMotsCles;
+    private String missionMetierCible;
+    private String missionCommentaires;
+    private String feedbackTuteur;
+    private String remarquesGenerales;
+}
